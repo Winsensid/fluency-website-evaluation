2761d237a32daf577cee31e2a664c886b74f2f3c
Win <49218880+Winsensid@users.noreply.github.com>
2026-01-08 22:22:29 -0500

    Fix import statement for FirefoxDriver

--- src/test/java/LinkVerificationTest.java
+++ src/test/java/LinkVerificationTest.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver; 
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
